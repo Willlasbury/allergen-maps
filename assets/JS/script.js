ed0a3d92af4c1462a520055791f4723f5152795b
Erick-McGee <erickmcgee3@gmail.com>
2023-04-11 11:27:05 -0700

    wee

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -77,7 +77,8 @@ function displayPlantInfo(e){
             // TODO: other locations
 
 let string = 'zion national park'
-let test = fetch(`https://developer.nps.gov/api/v1/passportstamplocations?q=${string}&limit=5&api_key=bh7IwlBKJxYuDvsGfVs2ogc9sumwDTYJJZi11Yea`).then(function(response){
+let test = fetch(`https://developer.nps.gov/api/v1/passportstamplocations?q=${string}&limit=5&api_key=bh7IwlBKJxYuDvsGfVs2ogc9sumwDTYJJZi11Yea`)
+.then(function(response){
     console.log("response.json():", response)
     response.json().then(function (data){
     console.log("data:", data)
